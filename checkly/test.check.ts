0509aa12e7d26331f44e3a0a02f6add1d5ac215f
Gérald Barré <meziantou@users.noreply.github.com>
2025-07-08 16:03:33 -0400

    wip

---
+++ checkly/test.check.ts
@@ -0,0 +1,18 @@
+import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
+
+const cortexTags: string[] = []; // Do not edit. Updated automatically at publish time
+
+new ApiCheck('dummy-api', {
+  name: 'dummy',
+  degradedResponseTime: 5000,
+  maxResponseTime: 10000,
+  tags: [...cortexTags],
+  request: {
+    method: 'GET',
+    url: 'https://dummy/health',
+    skipSSL: true,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200),
+    ]
+  }
+})
